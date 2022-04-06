cacac63b20fd051e076c06f8b0946bec73149800
Komsakorn Jiarawish <komsakorn.j@hotmail.com>
2022-04-06 18:39:51 +0700

    6/4/65

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -10,6 +10,13 @@ function Header() {
       <div className="flex flex-col">
         <code className="text-lightblue_vs">Hi, my name is</code>
         <code className="text-[#e6f1ff] text-7xl mt-5">Komsakorn Jiarawish</code>
+        <div className="text-[#a2aabc] text-lg mt-5 mx-12 flex flex-col items-center text-justify">
+        <code>
+        I'm a passionate software developer from Thailand, Currently lived in Bangkaen, Bangkok. 
+        I love and like to discover all of the coding knowledge that I never knew before. 
+        My goal is to become a senior developer that reliable and easy to reach for all of my new junior developers.
+        </code>
+      </div>
       </div>
       <button
         className="border border-lightblue_vs text-lightblue_vs mt-10 p-3 rounded hover:bg-opacity-10 hover:bg-lightblue_vs w-1/2"
